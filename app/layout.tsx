7f1e97473d303a2937ea5228234381021995e595
Atlas Acopian <atlasacopian@Mac.lan>
2024-11-28 19:16:18 -0800

    Update percentage rounding and fix deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,6 +7,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <style>{`
+          * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+          }
+          body {
+            background: white;
+          }
+          textarea {
+            border: none;
+            outline: none;
+            resize: none;
+            font-family: 'Courier', monospace;
+          }
+        `}</style>
+      </head>
       <body>{children}</body>
     </html>
   );
