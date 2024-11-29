fa5eb1a068a4665b46bf81ed2a991fb5c8f935f1
Atlas Acopian <atlasacopian@Mac.lan>
2024-11-28 19:19:38 -0800

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  distDir: '.next',
+  typescript: {
+    ignoreBuildErrors: false
+  }
 }
 
 module.exports = nextConfig
