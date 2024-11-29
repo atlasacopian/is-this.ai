523a73a61c9c0d08e97ad4cca3d6ad70ee01e8c8
Atlas Acopian <atlasacopian@Mac.lan>
2024-11-28 19:22:28 -0800

    Simplify next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  distDir: '.next',
-  typescript: {
-    ignoreBuildErrors: false
-  }
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
